8bf173d77c1d709ee3a457d9cd7f69cf9db826a0
Rupen97 <rupenpcb@gmail.com>
2025-11-11 19:28:32 +0545

    workshop 2 tasks

---
+++ WW03 Tasks/Task2.cs
@@ -0,0 +1,17 @@
+﻿class NullOperators
+{
+    public void PerformNullChecks()
+    {
+        String username = null;
+
+        String result = (username == null) ? "Username is not available" : username;
+        Console.WriteLine(result);
+
+        String result2 = username ?? "Username is not available";
+        Console.WriteLine(result2);
+
+        username ??= "rupen";
+        Console.WriteLine(username);
+    }
+
+}
---
+++ WW03 Tasks/Task3.cs
@@ -0,0 +1,22 @@
+﻿class AgeCheck
+{
+    public void checkAge(int  age)
+    {
+        if (age < 13)
+        {
+            Console.WriteLine("Child");
+        }
+        else if (age >= 13 && age <= 19 )
+        {
+            Console.WriteLine("Teenager");
+        }
+        else if ( age >= 20)
+        {
+            Console.WriteLine("Adult");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Age");
+        }
+    }
+}
